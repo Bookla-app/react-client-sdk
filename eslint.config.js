f4b63a3d58f83f1db0f766449b2874f85aa00b5c
Igors Nemenonoks <ios@chi.lv>
2024-11-18 16:33:41 +0100

    eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import { typescript } from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+    {
+        files: ["src/**/*.{ts,tsx}"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            "@typescript-eslint": typescript,
+        },
+        rules: {
+            // Your rules here
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        },
+    },
+];
